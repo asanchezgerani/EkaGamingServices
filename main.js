5da9f1a1225b67b8795d178cdf30ce92ab522ecc
Anthony <anthonymsg@hotmail.com>
2022-07-22 15:19:21 -0400

    Agregamos Slider Responsive

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const menu =  document.querySelector("nav");
 const menuImagen = document.querySelector(".menuHamburguesa");
+const sliders = [...document.querySelectorAll('.slider_body')];
+const arrowNext = document.querySelector('#next');
+const arrowBefore = document.querySelector('#before');
+let value;
+
+arrowNext.addEventListener('click',() =>changePosition(1));
+arrowBefore.addEventListener('click',() =>changePosition(-1));
 
 menuImagen.addEventListener("click", function(){
     if(menuImagen.classList.toggle(".activo"))
@@ -15,5 +22,19 @@ menuImagen.addEventListener("click", function(){
         console.log(menuImagen);
     }
 });
+
+function changePosition(change)
+{
+    const currentElement = Number(document.querySelector('.slider_body--show').dataset.id);
+    
+    value = currentElement;
+    value+= change;
+    if(value === 0 || value == sliders.length+1)
+    {
+        value = value === 0 ? sliders.length : 1;
+    }
+    sliders[currentElement- 1].classList.toggle('slider_body--show');
+    sliders[value - 1].classList.toggle('slider_body--show');
+}
 
 
